3af5c96c71cfb519dd34d1b9f9e476c5a14c6639
enimiste <nouni.elbachir@gmail.com>
2024-12-03 17:37:15 +0100

    play with DI with Spring using Profiles

--- src/main/java/guru/springframework/spring6di/controllers/Myi18NController.java
+++ src/main/java/guru/springframework/spring6di/controllers/Myi18NController.java
@@ -5,10 +5,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ConstructorInjectedController {
+public class Myi18NController {
+
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
+    public Myi18NController(@Qualifier("i18NService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
