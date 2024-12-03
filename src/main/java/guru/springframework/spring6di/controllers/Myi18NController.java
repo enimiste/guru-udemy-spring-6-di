a1e3896d1ca66c9951725040c9b4572959755874
enimiste <nouni.elbachir@gmail.com>
2024-12-03 17:55:17 +0100

    DI exercise

--- src/main/java/guru/springframework/spring6di/controllers/Myi18NController.java
+++ src/main/java/guru/springframework/spring6di/controllers/Myi18NController.java
@@ -2,9 +2,11 @@ package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Lazy
 public class Myi18NController {
 
     private final GreetingService greetingService;
--- src/main/java/guru/springframework/spring6di/services/GreetingServicePrimary.java
+++ src/main/java/guru/springframework/spring6di/services/GreetingServicePrimary.java
@@ -3,8 +3,8 @@ package guru.springframework.spring6di.services;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Service
 @Primary
+@Service
 public class GreetingServicePrimary implements GreetingService {
     @Override
     public String sayGreeting() {
