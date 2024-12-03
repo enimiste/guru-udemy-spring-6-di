e87a2a1c26e96b0b98331fcbe4edb6fb742feb3b
enimiste <nouni.elbachir@gmail.com>
2024-12-03 15:36:41 +0100

    play with DI without Spring

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,16 +1,12 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
-import guru.springframework.spring6di.services.GreetingServiceImpl;
-import org.springframework.stereotype.Controller;
 
-@Controller
-public class MyController {
-
+public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public MyController() {
-        this.greetingService = new GreetingServiceImpl();
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
     public String sayHello() {
