6e657a784c8e8c9651173a084fa64c6f5d488564
enimiste <nouni.elbachir@gmail.com>
2024-12-03 15:25:17 +0100

    play with beans DI

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -4,7 +4,6 @@ import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
